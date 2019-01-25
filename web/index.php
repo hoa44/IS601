defc01f39775e1b28dbbe174eccabcfc19c19173
Hub Arnold <hoa44>
2019-01-24 16:30:31 -0800

    Demo

--- web/index.php
+++ web/index.php
@@ -24,7 +24,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return str_repeat('Hello', getenv('TIMES'));
+  return str_repeat('Hello-', getenv('TIMES'));
 });
 
 $app->get('/cowsay', function() use($app) {
